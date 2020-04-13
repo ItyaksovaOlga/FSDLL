7bd98ca22d05af9238c440b5922490eb264fc8b3
dosmiii@outlook.com <dosmiii@outlook.com>
2020-04-13 22:38:57 +0500

    Добавьте файлы проекта.

---
+++ FSDll/FSDll.cpp
@@ -0,0 +1,120 @@
+﻿#include <Windows.h>
+#include <stdio.h>
+#include "pch.h"
+#include <iostream>
+#include "FSDLL.h" 
+
+using namespace std;
+
+void PrintInfo(NTFS pBoot)
+{
+    cout << "Sector size: " << pBoot.bytesPerSector << endl;
+    cout << "Sectors per cluster: " << pBoot.sectorsPerCluster << endl;
+    cout << "Volume descriptor: " << pBoot.mediaDescriptor << endl;
+    cout << "Sectors per track: " << pBoot.sectorsPerTrack << endl;
+    cout << "Number of heads: " << pBoot.numberOfHeads << endl;
+    cout << "Number of sectors: " << pBoot.numOfSectorsInTheVol << endl;
+    cout << "First cluster of MFT: " << pBoot.LCNofVCNofTheMFT << endl;
+    cout << "First cluster of MFT mirror: " << pBoot.LCNofVCNofTheMFTMirr << endl;
+    cout << "Clusters per MFT Record: " << pBoot.clustersPerMFTrecord << endl;
+    cout << "Clusters per Index Record: " << pBoot.clustersPerIndexRecord << endl;
+    cout << "Volume serial number: " << pBoot.volumeSerialNumber << endl;
+}
+
+
+string getNameFS(string fsName)
+{
+    char volumeNameBuffer[MAX_PATH];
+    char fileSystemNameBuffer[MAX_PATH];
+    DWORD volumeSerialNumber;
+    DWORD maximumComponentLength;
+    DWORD fileSystemFlags;
+
+    string rootPathName = fsName + ":\\";
+
+    //GetVolumeInformation bзвлекает информацию о файловой системе
+    if (GetVolumeInformationA(
+        rootPathName.c_str(),			//Указатель на строку, которая содержит корневой каталог тома, который будет описан
+        volumeNameBuffer,				//Указатель на буфер, который получает имя указанного тома
+        sizeof(volumeNameBuffer),		//Длина буфера имени тома
+        &volumeSerialNumber,			//Указатель на переменную, которая получает серийный номер тома
+        &maximumComponentLength,		//Компонент имени файла - это часть имени файла между обратными косыми чертами
+        &fileSystemFlags,				//Указатель на переменную, которая получает флаги, связанные с указанной файловой системой
+        fileSystemNameBuffer,			//Указатель на буфер, который получает имя файловой системы
+        sizeof(fileSystemNameBuffer)	//Длина буфера имени файловой системы
+    ))
+    {
+        cout << "File system is: " << fileSystemNameBuffer << endl;
+        string fileSystemName = fileSystemNameBuffer;
+        return fileSystemName;
+    }
+
+}
+
+//HANDLE - дескриптор, т.е. число, с помощью которого можно идентифицировать ресурс
+//В операционной системе Windows для получения дескриптора файла его необходимо открыть функцией CreateFile 
+//В Windows все объекты идентифицируются 32-разрядным целым числом, которое называется дескриптором
+
+bool GetInfo(string rootPathName, NTFS* pBoot)
+{
+    HANDLE fileHandle = CreateFileA(
+        rootPathName.c_str(),           //конвертируем имя файла из string для функции
+        GENERIC_READ,                       //Запрашиваемый доступ для чтения к файлу или устройству
+        //Позволяет последующим операциям открытия файла или устройства запрашивать доступ для чтения/записи/удаления
+        FILE_SHARE_READ | FILE_SHARE_WRITE,
+        NULL,
+        OPEN_EXISTING,                      //Открывает файл или устройство, только если оно существует
+        0,
+        NULL
+    );
+
+    //параметр для недопустимого значения дескриптора
+    if (fileHandle == INVALID_HANDLE_VALUE)
+    {
+        perror("Invalid handle value!!!");
+    }
+
+    //Позиционирование на нулевом смещении
+    LARGE_INTEGER sectorOffset;
+    sectorOffset.QuadPart = 0;
+
+    //Перемещает указатель файла
+    unsigned long currentPosition = SetFilePointer(
+        fileHandle,                                     // дескриптор файла
+        sectorOffset.LowPart,                           //байты перемещения указателя
+        &sectorOffset.HighPart,                         //байты перемещения указателя
+        FILE_BEGIN                                      //Начальная точка - ноль или начало файла.
+    );
+
+    if (currentPosition != sectorOffset.LowPart)
+    {
+        perror("Error!!!");
+    }
+
+    //Выделение памяти в стеке
+    BYTE dataBuffer[1024];
+
+    //Результат считывания байтов
+    DWORD bytesRead;
+
+    //чтение
+    bool readResult = ReadFile(
+        fileHandle,
+        dataBuffer,
+        1024,
+        &bytesRead,
+        NULL
+    );
+
+    if (!readResult || bytesRead != 1024)
+    {
+        perror("Error!!!! ");
+    };
+
+    //приведение типов
+    pBoot = reinterpret_cast<NTFS*>(dataBuffer);
+
+    CloseHandle(fileHandle);
+
+    return true;
+};
---
+++ FSDll/FSDll.h
@@ -0,0 +1,43 @@
+﻿#pragma once
+
+//Making DLL exportable and importable
+#ifdef FSDLL_EXPORTS
+#define FSDLL_API __declspec(dllexport)
+#else
+#define FSDLL_API __declspec(dllimport)
+#endif
+
+#include <iostream>
+#include <windows.h>
+#include <stdio.h>
+using namespace std;
+
+#pragma pack(1)
+
+typedef struct
+{
+	BYTE    jump[3];                        //переход на программу начальной загрузки
+	BYTE    systemID[8];                    //операционная система и версия
+	UINT16  bytesPerSector;                 //размер сектора в байтах
+	UINT16  sectorsPerCluster;              //количество секторов в одном кластере
+	BYTE    reserv1[6];                     //зарезервировано
+	UINT16  mediaDescriptor;                //тип носителя данных
+	BYTE    reserv2;                        //зарезервировано
+	UINT16  sectorsPerTrack;                //количество секторов на дорожке
+	UINT16  numberOfHeads;                  //количество магнитных головок
+	BYTE    reserv3[8];                     //зарезервировано
+	UINT32  defaultValue;                   //содержит число 0х80
+	UINT64  numOfSectorsInTheVol;           //количество секторов
+	UINT64  LCNofVCNofTheMFT;               //lcn - логический номер кластера
+	UINT64  LCNofVCNofTheMFTMirr;           //vcn - виртуальный номер кластера
+	DWORD   clustersPerMFTrecord;           //размер записи mft в кластерах
+	UINT32  clustersPerIndexRecord;         //размер буфера индексов в кластерах
+	UINT64  volumeSerialNumber;             //серийный номер тома
+} NTFS;
+
+#pragma pack()
+
+//External linkage of DLL functions
+extern "C++" FSDLL_API bool GetInfo(string rootPathName, NTFS * pBoot);
+extern "C++" FSDLL_API void PrintInfo(NTFS pBoot);
+extern "C++" FSDLL_API string getNameFS(string fileName);
